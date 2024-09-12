5012c49c4dbc8c14696b57497b3487b5b0a516a9
azisssss <aziswh7@gmail.com>
2024-09-12 13:41:40 +0700

    backend setup

---
+++ backend/database.js
@@ -0,0 +1,26 @@
+// const { response } = require("express");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  password: "kivandra7",
+  port: 5432,
+  database: "choolifah",
+});
+
+// const createTblQry = "CREATE TABLE users (user_id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) UNIQUE NOT NULL);";
+
+// pool
+//   .query(createTblQry)
+//   .then((Response) => {
+//     console.log("Table created successfully!");
+//     console.log(response);
+//     pool.end();
+//   })
+//   .catch((err) => {
+//     console.log("Error creating database", err);
+//     pool.end();
+//   });
+
+module.exports = pool;
