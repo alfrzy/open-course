311fa9997cfc58ebeb1ee38cbdf47749626e0fc9
adis.kasenda <adis.kasenda@gmail.com>
2024-09-24 17:12:17 +0700

    page change password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Testing from "./pages/Testing";
 import Home from "./pages/Home";
+import ChangePassword from "./pages/ChangePassword/changePassword";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/testing" element={<Testing />} />
+        <Route path="/change-password" element={<ChangePassword />} />
       </Routes>
     </Router>
   );
