9c49ef3a56347a2f98e216cf20ca4548d6e83eeb
azisssss <aziswh7@gmail.com>
2024-10-01 16:26:46 +0700

    delete checkrole.js and api url changes on backend

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,8 +2,6 @@ const { Router } = require("express");
 
 const { verifyToken } = require("../cores/authMiddleware");
 
-// const checkRole = require("../cores/checkRole");
-
 const userApiController = require("../controllers/user");
 const bookApiController = require("../controllers/book");
 
@@ -11,11 +9,5 @@ const api = Router();
 
 api.use("/api/v1/user", userApiController);
 api.use("/api/v1/book", bookApiController);
-
-//contoh
-// api.use("/api/v1/dosen", verifyToken, checkRole([1]), dosenApiController);
-// api.use("/api/v1/admin", verifyToken, checkRole([2]), (req, res) => {
-//     res.send("Admin Access Only");
-//   });
 
 module.exports = api;
