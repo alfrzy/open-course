255a1824cc605a8265a269c2948ce965eb23bf70
adis.kasenda <adis.kasenda@gmail.com>
2024-10-03 14:46:10 +0700

    tampilan detai kelas di landing page

---
+++ frontend/src/components/Footer/LandingFooter.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { FaFacebookF, FaYoutube, FaTwitter, FaGoogle, FaRss } from "react-icons/fa";
+import { FiPhone, FiMail, FiMapPin } from "react-icons/fi";
+import { BsWhatsapp } from "react-icons/bs";
+
+const LandingFooter = () => {
+  return (
+    <footer className="bg-blue-700 text-white font-poppins md:px-10">
+      <div className="container mx-auto py-8 px-5">
+        <div className="flex flex-col md:flex-row justify-between">
+          {/* left */}
+          <div className="mb-6 md:mb-0">
+            <h3 className="font-bold text-lg">FE Open Course</h3>
+            <p className="mt-4 mb-2">Informasi Lebih Lanjut:</p>
+            <div className="flex space-x-3">
+              <a href="#" className="text-white hover:text-gray-300">
+                <FaFacebookF />
+              </a>
+              <a href="#" className="text-white hover:text-gray-300">
+                <FaYoutube />
+              </a>
+              <a href="#" className="text-white hover:text-gray-300">
+                <FaTwitter />
+              </a>
+              <a href="#" className="text-white hover:text-gray-300">
+                <FaGoogle />
+              </a>
+              <a href="#" className="text-white hover:text-gray-300">
+                <FaRss />
+              </a>
+            </div>
+          </div>
+
+          {/* right */}
+          <div>
+            <h3 className="font-bold text-lg">Kontak Kami:</h3>
+            <ul className="mt-4 space-y-2">
+              <li className="flex items-center">
+                <FiMapPin className="mr-2" />
+                Jl. Raya Kaligawe Km.4 Semarang 50112
+              </li>
+              <li className="flex items-center">
+                <FiPhone className="mr-2" />
+                (024) 6583584 ext 537
+              </li>
+              <li className="flex items-center">
+                <BsWhatsapp className="mr-2" />
+                081329262505
+              </li>
+              <li className="flex items-center">
+                <FiMail className="mr-2" />
+                fe@unissula.ac.id
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <hr className="my-6 border-white" />
+
+        {/* bottom */}
+        <div className="flex flex-col md:flex-row justify-between items-center text-xs">
+          <p>copyright@2020</p>
+          <p>
+            Dibuat penuh dengan <span className="text-red-500">❤</span> oleh Azuralabs
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default LandingFooter;
--- frontend/src/pages/kelas/DetailKelas.jsx
+++ frontend/src/pages/kelas/DetailKelas.jsx
@@ -1,10 +1,92 @@
 import React from 'react';
-import DetailKelas from '../../components/DetailKelas/detailKelas';
+import Navbar from '../../components/Navbar/Navbar';
+import InstrukturCard from '../../components/DetailKelas/InstrukturCard';
+import LandingFooter from '../../components/Footer/LandingFooter';
+import ComponentButton from '../../components/Button/ComponentButton';
+
+
 
 const Detail = () => {
   return (
-    <div>
-      <DetailKelas />
+  <div className="min-h-screen font-poppins bg-gray-200">
+      <div className="flex flex-col sm:flex-row items-center justify-center lg:justify-start mt-4">
+      <div className="w-full">
+      {/* Header Section */}
+      <Navbar/>
+      <div className="bg-gray-100 p-4 sm:p-6 lg:p-10 border-b">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row items-center justify-between">
+            {/* Teks Header */}
+            <div className="w-full lg:w-1/2 text-center lg:text-left mb-6 lg:mb-0">
+              <h1 className="text-2xl sm:text-3xl text-blue-500 font-bold">Manajemen Keuangan Pribadi</h1>
+              <p className="mt-2 text-sm sm:text-base">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+              </p>
+              <div className="flex flex-col sm:flex-row items-center justify-center lg:justify-start mt-4">
+              <ComponentButton text={"Daftar Sekarang"} color={"bg-blue-600"} link={"/"} />
+                <p className="mt-2 sm:mt-0 sm:ml-4 text-gray-700 text-sm">814 telah mendaftar</p>
+              </div>
+            </div>
+
+            {/* Gambar */}
+            <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+              <img
+                src="https://blog-cdn.kitalulus.com/blog/wp-content/uploads/2024/02/20154131/626e2322c1b6ca1cf2a84104_Cara20mengelola20keuangan20pribadi20dengan20gaji20kecil.jpg"
+                alt="Manajemen Keuangan"
+                className="max-w-xs w-full h-auto rounded"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Tentang Kelas Section */}
+      <div className="p-4 sm:p-6 lg:p-10 border-b">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row justify-between items-start">
+            {/* Bagian Teks */}
+            <div className="lg:w-2/3 mb-6 lg:mb-0">
+              <h2 className="text-xl sm:text-2xl text-blue-500 font-bold">Tentang Kelas Ini</h2>
+              <p className="text-gray-600 text-sm">1.201 Dilihat</p>
+              <p className="mt-4 text-gray-700 text-sm sm:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+              </p>
+              <p className="mt-4 text-gray-700 text-sm sm:text-base">
+                Suspendisse iaculis, dolor ac tempus iaculis, erat mauris...
+              </p>
+            </div>
+
+            {/* Box */}
+            <div className="bg-gray-100 p-4 rounded-md w-full lg:w-auto lg:min-w-[250px]">
+              <ul className="list-inside text-sm">
+                <li className="border-b py-2"><strong>Lama:</strong> 4 Minggu</li>
+                <li className="border-b py-2"><strong>Upaya:</strong> 2-3 jam per minggu</li>
+                <li className="border-b py-2"><strong>Harga:</strong> GRATIS</li>
+                <li className="border-b py-2"><strong>Bahasa:</strong> Indonesia</li>
+                <li className="py-2"><strong>Transkrip Video:</strong> Indonesia</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Apa yang Akan Dipelajari Section */}
+      <div className="bg-gray-100 p-4 sm:p-6 lg:p-10 border-b">
+        <div className="container mx-auto">
+          <h3 className="text-lg sm:text-xl text-blue-500 font-bold">Apa yang akan Kamu pelajari:</h3>
+          <ul className="mt-4 list-inside list-disc text-sm sm:text-base">
+            <li>✔ Duis ultricies porttitor commodo.</li>
+            <li>✔ Suspendisse iaculis, dolor ac tempus iaculis...</li>
+            <li>✔ Suspendisse iaculis, dolor ac tempus iaculis...</li>
+          </ul>
+        </div>
+      </div>
+      <div className='border-b'>
+          <InstrukturCard />
+          </div>
+    </div>
+  </div>
+      <LandingFooter/>
     </div>
   );
 }
