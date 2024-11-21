2826d54688bdb48c84bee92197abd2cbf73842d8
azisssss <aziswh7@gmail.com>
2024-11-21 14:48:47 +0700

    migration changes

---
+++ backend/migrations/sequelize/20241023114003-add_status_to_users.js
@@ -0,0 +1,12 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Users", "status", {
+      type: Sequelize.BOOLEAN,
+      allowNull: true,
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("Users", "status");
+  },
+};
